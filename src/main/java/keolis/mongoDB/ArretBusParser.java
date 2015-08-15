a5de76b0caee16dbb400cb343d48c08f58b77bd6
Yassine Cherif <cherif.esir@gmail.com>
2015-08-15 20:42:35 +0200

    correct diff date

--- src/main/java/keolis/mongoDB/ArretBusParser.java
+++ src/main/java/keolis/mongoDB/ArretBusParser.java
@@ -117,7 +117,7 @@ public class ArretBusParser {
 					Date dateExpected 	= sdf.parse(value.getJSONObject("@attributes").getString("expected"));
 					Date dateReel 	= sdf.parse(value.getString("content"));
 					//soustraction des deux dates
-					long diff_TR = dateExpected.getTime() - dateReel.getTime();
+					long diff_TR = dateReel.getTime() - dateExpected.getTime();
 
 					arretBusMongo.append(ArretBusAttribut.DIFF_TR, Long.toString(diff_TR/1000));
 					basicDBObjects.add(arretBusMongo);
